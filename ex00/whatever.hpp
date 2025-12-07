8cec1369b5b4f213af1547e3fcf31c9e77966dee
Noam VOLTAIRE <noam.voltaire@gmail.com>
2025-12-07 16:25:20 +0100

    added test and comments

--- ex00/whatever.hpp
+++ ex00/whatever.hpp
@@ -1,7 +1,6 @@
 #ifndef WHATEVER_HPP
 #define WHATEVER_HPP
 
-//#include <cstddef>
 #include <iostream>
 
 template <typename T>
@@ -22,24 +21,6 @@ const T	&min(const T &a, const T &b)
 		return (b);
 };
 
-template <size_t L1, size_t L2>
-const char	(*min(const char (&a)[L1], const char (&b)[L2]))
-{
-	if(strcmp(a,b) < 0)
-		return (a);
-	else
-		return (b);
-};
-
-template <size_t L>
-const char	(*min(const char (&a)[L], const char (&b)[L]))
-{
-	if (a < b)
-		return (a);
-	else
-		return (b);
-};
-
 template <typename T>
 const T	max(const T &a, const T &b)
 {
@@ -49,6 +30,32 @@ const T	max(const T &a, const T &b)
 		return (b);
 };
 
+
+//did min and max for string literals ("str") not asked for but 
+// can be done trough templates like below
+// since "hello" and "hi" are actually diff types
+// const char[6], const char[3]
+
+
+template <size_t L1, size_t L2>
+const char	(*min(const char (&a)[L1], const char (&b)[L2]))
+{
+	if(strcmp(a,b) < 0)
+		return (a);
+	else
+		return (b);
+};
+
+// needed for same length string literal comparaison 
+
+template <size_t L>
+const char	(*min(const char (&a)[L], const char (&b)[L]))
+{
+	if (a < b)
+		return (a);
+	else
+		return (b);
+};
 
 template <size_t L1, size_t L2>
 const char	(*max(const char (&a)[L1], const char (&b)[L2]))
